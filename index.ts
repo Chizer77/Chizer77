7c063bfd319bb936dbc82114ff00ff93bbfa020e
0x7C00H <97499725+Chizer77@users.noreply.github.com>
2025-05-03 03:01:11 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ function generateArtworksHtml(list: any[]) {
         </tr>`, ``)
 
     return minifyHTML
-        `<table align="center">
+        `<table>
             <thead align="center">
                 <tr border: none;>
                     <td><b>🎉 Project</b></td>
